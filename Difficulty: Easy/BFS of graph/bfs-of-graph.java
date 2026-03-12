f73f0548f482c482d535922405bfa7df7108be1c
Prasanna Patil <proplayer14102004@gmail.com>
2026-03-12 06:30:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,21 +1,25 @@
-import java.util.*;
 class Solution {
     public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
-          Queue<Integer>q=new LinkedList<>();
-          ArrayList<Integer>list=new ArrayList<>();
-          boolean[]vis=new boolean[adj.size()];
-          vis[0]=true;
-          q.add(0);
-          while(!q.isEmpty()){
-              Integer node=q.poll();
-              list.add(node);
-              for(int i: adj.get(node)){
-                  if(vis[i]==false){
-                      vis[i]=true;
-                      q.add(i);
-                  }
-              }
-          }
-          return list;
+        // initial: Q
+        // Q always has starting node
+        Queue<Integer>q=new LinkedList<>();
+        boolean[]vis=new boolean[adj.size()];
+        q.add(0);
+        vis[0]=true;
+        
+        
+        ArrayList<Integer> list=new ArrayList<>();
+        //keep taking it out till its not empty
+        while(!q.isEmpty()){
+             Integer node=q.poll();
+             list.add(node);
+             for(int i:adj.get(node)){
+               if(!vis[i]){
+                   vis[i]=true;
+                q.add(i);
+              } 
+            }
+        }
+       return list;
     }
 }
